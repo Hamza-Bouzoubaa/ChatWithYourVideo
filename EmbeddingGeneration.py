2938f1e16b0b1844792bda1b1071a2f6067e6f9c
Hamza <104928656+Hamza-Bouzoubaa@users.noreply.github.com>
2023-11-30 13:32:00 +0100

    1-Added Transcription using Whsiper X

---
+++ EmbeddingGeneration.py
@@ -0,0 +1,63 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import AzureOpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def SplitResumeText(Text,chunk_size=5000,chunk_overlap = 00,separators = ["\n\n"]):
+
+    rec_text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size= chunk_size,
+    chunk_overlap = chunk_overlap,
+    length_function=len,
+    separators = separators
+
+    )
+
+    chunks = rec_text_splitter.split_text(Text)
+
+    return chunks
+
+def ReadFile(FilePath):
+    # Get the file extension
+    file_extension = FilePath.split('.')[-1].lower()
+
+    
+    if file_extension == 'txt':
+        with open(FilePath, 'r', encoding='utf-8') as file:
+            raw_text = file.read()
+            
+    
+
+    else:
+        raise ValueError(f"Unsupported file format: {file_extension}")
+
+    return raw_text
+
+
+
+
+def GenerateEmbeddings(FilePath):  #Works with PDF DOCX and TXT
+
+    Transcription = ReadFile(FilePath)  #Extracting raw text from Resume
+    
+    
+    TranscriptionSections = SplitResumeText(Transcription)  # splitting the raw text into sections
+
+    embeddings =  AzureOpenAIEmbeddings(model = "text-embedding-ada-002")  # Setting up OpenAI embedding
+
+    
+    dbTranscription  = FAISS.from_texts(TranscriptionSections,embeddings)      # Generating and storing Resume embeddings 
+    
+
+    return dbTranscription
+
+
+def QuerySimilaritySearch(Query,db,k=1):
+    similar = db.similarity_search(Query,k)
+    page_content_array = [doc.page_content for doc in similar]
+    return page_content_array
--- Transcription.py
+++ Transcription.py
@@ -11,7 +11,6 @@ load_dotenv()
 hf_token = os.getenv('hf_token')
 
 
-
 def transcribe_with_diarization(audio_file_path,OutputFile, model_type="large-v2", device="cpu", hf_token=hf_token):
     # Step 1: Load the WhisperX model with float32 compute type
     model = whisperx.load_model(model_type, device, compute_type="float32")
@@ -36,91 +35,24 @@ def transcribe_with_diarization(audio_file_path,OutputFile, model_type="large-v2
     result = whisperx.assign_word_speakers(diarize_segments, result)
 
     transcription = result["segments"]
-
-    unique_speakers = set(word.get('speaker', 'UNKNOWN') for word in transcription)
-
-    # Count total number of unique speakers
-    total_speakers = len(unique_speakers)
-
-    speaker_dict = {speaker: [] for speaker in unique_speakers}
-
-
-
-    audio = AudioSegment.from_mp3(audio_file_path)
-    for i in range(len(transcription)):
-        sentence = transcription[i]
-
-        text = sentence["text"]
-        speaker = sentence["speaker"]
-        start = sentence["start"] 
-        end = sentence["end"] 
-
-        speaker_dict[speaker].append(f"segment_{speaker}_{i}.wav")
-
-        segment =  audio[start  * 1000 :end  * 1000]
-        segment_filename = os.path.join("sub", f"segment_{speaker}_{i}.wav")
-        segment.export(segment_filename, format="wav")
-        
-        Row = {"file" : f"sub/segment_{speaker}_{i}.wav","speaker": speaker, "start":start, "end":end,"utterance":text}
-        df.loc[len(df)] = Row  # detailed frame
-        text = ""
-        startList = []
-        for k in range(len(sentence["words"])):
-
-            word = sentence["words"][k]
-
-            text += " "+word["word"]
-            try:
-                speaker = word["speaker"]
-                start = word["start"] 
-                end = word["end"]
-            except:
-                
-                #end = sentence["words"][k+1]["start"] 
-                try:
-                    print("couldn't catch timing")
-                    print(word,sentence)
-                    speaker = sentence["words"][k-1]["speaker"]
-                    start = sentence["words"][k-1]["end"] 
-                    end = sentence["words"][k+1]["start"] 
-                except:
-                    end = sentence["words"][k-1]["end"]
-                
-            try:
-                timediff = sentence["words"][k+1]["start"] - end 
-
-            except:
-                timediff = 1
-                
-
-        
-            
-
-
     
-
-        # Export the combined audio to a file
-        #combined_audio.export(f"CombinedAudio/{speaker}.mp3", format="mp3")
-        
-
-
-    df.to_excel(DetailedFrame,index = False)
-    df2.to_excel(SubDetailedFrame,index = False)
-
-
-
-
-#DetailedFrame
-#SubDetailedFrame
-
-                       
-
-        
+    text = transcription[0]["speaker"]+": \n" 
+    for i in range(len(transcription)):
+    
+    
+    
+        if (i>0  and (transcription[i-1]["speaker"] != transcription[i]["speaker"])):
+            text += "\n\n"+transcription[i]["speaker"]+": \n"
+        text  += transcription[i]["text"]
+    
+      
+    # Write the text to the file
+    with open(OutputFile, "w", encoding="utf-8") as file:
+        file.write(text)
+    
+    print(f"Text has been saved to {OutputFile}")
 
 
- 
-# Example usage:
-#audio_file_path = "Audio.wav"
-#transcription = transcribe_with_diarization(audio_file_path)
+    return transcription
 
-
+transcribe_with_diarization("audio.wav","transcription.txt")
